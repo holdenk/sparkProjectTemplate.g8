bb2e3bba9a363eb2c1b82649207e6ea7830c7ae0
greg <greg.nwosu@gmail.com>
2017-10-12 18:10:50 +0100

    updated to use Datasets.

--- src/main/g8/src/main/scala/$package__packaged$/WordCount.scala
+++ src/main/g8/src/main/scala/$package__packaged$/WordCount.scala
@@ -3,26 +3,26 @@ package $organization$.$name$
 /**
  * Everyone's favourite wordcount example.
  */
-
-import org.apache.spark.rdd._
+import org.apache.spark.sql.Dataset
 
 object WordCount {
   /**
    * A slightly more complex than normal wordcount example with optional
    * separators and stopWords. Splits on the provided separators, removes
    * the stopwords, and converts everything to lower case.
-   */
-  def withStopWordsFiltered(rdd : RDD[String],
-    separators : Array[Char] = " ".toCharArray,
-    stopWords : Set[String] = Set("the")): RDD[(String, Int)] = {
+    */
 
-    val tokens: RDD[String] = rdd.flatMap(_.split(separators).
-      map(_.trim.toLowerCase))
+  def withStopWordsFiltered(ds: Dataset[String],
+    separators : Array[Char] = " ".toCharArray,
+    stopWords : Set[String] = Set("the")) = {
+    val spark = ds.sparkSession
+    import spark.implicits._
+    val tokens = ds.flatMap(_.split(separators).map(_.trim.toLowerCase))
     val lcStopWords = stopWords.map(_.trim.toLowerCase)
     val words = tokens.filter(token =>
       !lcStopWords.contains(token) && (token.length > 0))
-    val wordPairs = words.map((_, 1))
-    val wordCounts = wordPairs.reduceByKey(_ + _)
+    val wordPairs :Dataset[(String,Int)]= words.map((_, 1))
+    val wordCounts = wordPairs.rdd.reduceByKey(_ + _)
     wordCounts
   }
 }
--- src/main/g8/src/test/scala/$package__packaged$/WordCountTest.scala
+++ src/main/g8/src/test/scala/$package__packaged$/WordCountTest.scala
@@ -5,24 +5,25 @@ package $organization$.$name$
  */
 
 import com.holdenkarau.spark.testing.SharedSparkContext
+import org.apache.spark.sql.SQLContext
 import org.scalatest.FunSuite
 
 class WordCountTest extends FunSuite with SharedSparkContext {
-  test("word count with Stop Words Removed"){
-    val linesRDD = sc.parallelize(Seq(
-      "How happy was the panda? You ask.",
-      "Panda is the most happy panda in all the#!?ing land!"))
+  test("countWords should  produce expected behaviours"){
+    val spark =new SQLContext(sc).sparkSession
+    import spark.implicits._
 
+    val linesDs = Seq(
+      "How happy was the panda? You ask.",
+      "Panda is the most happy panda in all the#!?ing land!").toDS
     val stopWords: Set[String] = Set("a", "the", "in", "was", "there", "she", "he")
     val splitTokens: Array[Char] = "#%?!. ".toCharArray
-
-    val wordCounts = WordCount.withStopWordsFiltered(
-      linesRDD, splitTokens, stopWords)
-    val wordCountsAsMap = wordCounts.collectAsMap()
-    assert(!wordCountsAsMap.contains("the"))
-    assert(!wordCountsAsMap.contains("?"))
-    assert(!wordCountsAsMap.contains("#!?ing"))
-    assert(wordCountsAsMap.contains("ing"))
-    assert(wordCountsAsMap.get("panda").get.equals(3))
+    val actualResultDs = WordCount.withStopWordsFiltered(linesDs, splitTokens, stopWords)
+    val actualResultMap = actualResultDs.collectAsMap()
+    assert(!actualResultMap.contains("the"))
+    assert(!actualResultMap.contains("?"))
+    assert(!actualResultMap.contains("#!?ing"))
+    assert(actualResultMap.contains("ing"))
+    assert(actualResultMap.get("panda").get.equals(3))
   }
 }
